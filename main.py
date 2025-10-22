8fe8785f5efa7c8048f1e4d7756b2ca586ba594c
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-10-22 18:02:19 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+""" Crie um programa que leia a temperatura do churrasco
+e dependendo da temperatura digitada ela retorne qual é o
+ponto do cozimento da carne """
+
+
+temperatura = int(input("Digite a temperatura da carne: "))
+
+if temperatura >= 48 or temperatura <54:
+    print("A carne está selada")
+elif temperatura >=54 or temperatura < 60:
+    print("A carne está no ponto")
+else:
+    print("A carne está bem passada")

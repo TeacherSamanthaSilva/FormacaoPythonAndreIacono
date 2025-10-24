cf81960b1f2cb607771b6e250d09667545e245ea
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-10-24 18:05:40 -0300

    Update desafio3.py

--- desafio3.py
+++ desafio3.py
@@ -7,3 +7,18 @@ Funcionarios = ["Thiago","Guilherme","Gabriel","Jader","Suzana","Kevin","Felipe"
 turnododia = ["Thiago","Guilherme","Gabriel","Jader"]
 turnodanoite = ["Suzana","Kevin","Felipe"]
 temcarro = ["Thiago","Guilherme","Kevin","Felipe"]
+
+#Lista 1 - Funcionários que tem carro e trabalham a noite
+
+lista1 = set(temcarro).intersection(turnodanoite)
+print(lista1)
+
+#Lista 2 - Funcionários que tem carro e trabalham durante o dia
+
+lista2 = set(temcarro).intersection(turnododia)
+print(lista2)
+
+#Lista 3 - Funcionários que não tem carro
+
+lista3 = set(Funcionarios).difference(temcarro)
+print(lista3)

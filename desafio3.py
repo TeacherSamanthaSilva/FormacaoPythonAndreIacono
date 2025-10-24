a8931607cb8a139df8ee7f282976b5fe208a016b
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-10-24 17:56:48 -0300

    Create desafio3.py

---
+++ desafio3.py
@@ -0,0 +1,9 @@
+""" Crie um programa que gere três listas de acordo com as necessidades abaixos
+Lista 1 - Funcionários que tem carro e trabalha a noite
+Lista 2 - Funcionários que tem carro e trabalham durante o dia
+Lista 3 - Funcionários que não tem carro """
+
+Funcionarios = ["Thiago","Guilherme","Gabriel","Jader","Suzana","Kevin","Felipe"]
+turnododia = ["Thiago","Guilherme","Gabriel","Jader"]
+turnodanoite = ["Suzana","Kevin","Felipe"]
+temcarro = ["Thiago","Guilherme","Kevin","Felipe"]

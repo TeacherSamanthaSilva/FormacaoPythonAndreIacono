894516d6b9124d78d711d6dce99413c1aeff9673
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-10-26 18:38:51 -0300

    Create desafio4.py

---
+++ desafio4.py
@@ -0,0 +1,7 @@
+""" Crie um programa para calcular o índice de massa corporal de uma pessoa
+
+se o indice for menor que 18.5 está muito baixo
+se tiver entre 18.5 e 24.9 está normal 
+se tiver entre 25 e 29.9 está com sobrepeso
+se tiver entre 30 e 39.9 a pessoa está com obesidedade
+se tiver acima de 40 a pessoa está com obesidade grave """

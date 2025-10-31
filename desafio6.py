298be9bb522feb177ebe4dcec3d724bb62db00e3
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-10-30 23:29:06 -0300

    Update desafio6.py

--- desafio6.py
+++ desafio6.py
@@ -2,3 +2,7 @@
 primeiro nome e outra chamada idade que deve guardar sua idade como um número inteiro 
 Depois imprime na tela a seguinte frase "Olpa Mundo! Meu nome é {nome} e tenho tal {idade}
 Substitua as variáveis pelo seu nome e idade"""
+
+nome = str(input("Digite o seu nome: "))
+idade = int(input("Digite a sua idade: "))
+print(f"Olá mundo! Meu nome é {nome} e tenho {idade} anos")

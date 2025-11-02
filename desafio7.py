209f5d101404e361d23b27e246852b5149d36c30
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-11-02 17:40:17 -0300

    Update desafio7.py

---
+++ desafio7.py
@@ -0,0 +1,8 @@
+# Neste desafio peço que você declare duas variáveis: num 1 que deve guardar o número inteiro 10 e num2 
+# que deve guardar o ponto flutuante 3.5. Depois disso eu quero que você divida num1 por nume e imprima
+# o resultado
+
+num1 = 10
+num2 = 3.5
+resultado = num1 / num2
+print(f"O resultado da divisão de {num1} e {num2} é {resultado}")

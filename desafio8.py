8d4ef8479aa51efa5bfe8ad0a352525533808b1f
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-11-04 23:16:55 -0300

    Update desafio8.py

--- desafio8.py
+++ desafio8.py
@@ -1,2 +1,6 @@
 #Crie um script que pergunte o nome e a idade do usuário usando a funçao input. Depois disso o script
 # deve imprimiar uma mensagem que diga Olá {nome} você tem {idade} anos
+
+nome = str(input("Digite o seu nome: "))
+idade = int(input("Digite a sua idade: "))
+print(f"Olá {nome} você tem {idade} anos")

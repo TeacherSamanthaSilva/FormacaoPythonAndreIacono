b1dde53aa07ad820510295c2cac7e28c7f7fb907
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-11-06 23:56:10 -0300

    Update desafio9.py

---
+++ desafio9.py
@@ -0,0 +1,14 @@
+#Crie um script que leia dois números e mostra a soma, a subtração, a multiplicação e a divisão desses números
+
+num1 = int(input("Digite um número qualquer "))
+num2 = int(input("Digite um número qualquer "))
+
+adicao = num1 + num2
+subtracao = num1 - num2
+multiplicacao = num1 * num2
+divisao = num1 / num2
+
+print(f"A adicao de {num1} + {num2} é {adicao}")
+print(f"A subtracao de {num1} + {num2} é {subtracao}")
+print(f"A multiplicacao de {num1} + {num2} é {multiplicacao}")
+print(f"A divisao de {num1} + {num2} é {divisao}")

0c0983180579a0b1d0e7b161e54985f56f65deb4
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-11-09 22:50:50 -0300

    Create desafio11.py

---
+++ desafio11.py
@@ -0,0 +1,9 @@
+#Agora mostre o primeiro e o último elemento da lista
+
+minha_lista = ["maçã", "banana", "cereja", "damasco"]
+
+# Imprime o primeiro elemento
+print(minha_lista[0]) 
+
+# Imprime o último elemento
+print(minha_lista[-1]) 

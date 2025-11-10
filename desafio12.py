43a0180571d0a79dc242ab6756406e3a20f4b33f
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-11-10 18:09:03 -0300

    Update desafio12.py

--- desafio12.py
+++ desafio12.py
@@ -1,1 +1,10 @@
 #Altere o segundo elemento da lista do desafio 11 de banana para morango e adicione abacaxi ao final da lista
+
+frutas = ["maça","banana","manga","uva"]
+
+frutas[1] = "morango"
+
+frutas.append("abacaxi")
+
+print(frutas)
+

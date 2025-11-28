5cafdd69f59e2ad0dcdcead6e10eb205e09c981e
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-11-28 00:52:00 -0300

    Create desafio14.py

---
+++ desafio14.py
@@ -0,0 +1,4 @@
+#Utilizando a lista de frutas do desafio anterior. Use um for loop para percorrer a lista e imprima cada
+#fruta precedida do texto "Eu gosto de"
+
+frutas = ["maça","banana","manga","uva","morango","abacaxi"]

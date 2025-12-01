7e983a775d9ac43fdd8711cad6f0ebc34553e924
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-12-01 18:57:50 -0300

    Create desafio16.py

---
+++ desafio16.py
@@ -0,0 +1,3 @@
+""" Para este desafio crie uma lista de frutas e outra de vegetais . Use um Nested for loop (For loop
+aninhado )  para imprimir todas as combinações possíveis de frutas e vegetais, com a fruta primeiro
+e o vegetal em segundo. """

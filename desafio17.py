63455b9997e220f33b23f45df87b7daa94a39482
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-12-03 00:20:30 -0300

    Update desafio17.py

---
+++ desafio17.py
@@ -0,0 +1,9 @@
+""" Para este desafio quero que você use um loop (mas não pode ser o for loop) para imprimir os números
+de um a dez na tela"""
+
+
+i = 0 
+
+while i < 11:
+    print(i)
+    i = i+ 1 

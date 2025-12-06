09d5af2cb0c662a68b15ff555cd7bce8238ea8b2
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-12-05 22:41:25 -0300

    Create desafio19.py

---
+++ desafio19.py
@@ -0,0 +1,2 @@
+""" Para este desafio crie uma lista de frutas e inclua maçã três vezes e outras frutas da sua escolha .
+Use um loop for para contar quantas maça aparece na lista e imprima o resultado """

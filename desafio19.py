ac2266bbffa42929fa1ecbf85d5eac63f4419219
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-12-05 22:48:56 -0300

    Update desafio19.py

--- desafio19.py
+++ desafio19.py
@@ -1,2 +1,15 @@
 """ Para este desafio crie uma lista de frutas e inclua maçã três vezes e outras frutas da sua escolha .
 Use um loop for para contar quantas maça aparece na lista e imprima o resultado """
+
+contador = 0
+
+frutas = ["maçã", "uva","maçã","banana","maçã" ,"manga"]
+
+for fruta in frutas:
+    if fruta == "maçã":
+        contador  += 1
+    print(fruta)
+
+print(f"A quantidade de maçãs é {contador}")
+
+

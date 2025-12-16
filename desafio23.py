3e76145a4b052cee61b90a65bdfe6ce3f6b3eaea
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-12-16 19:02:16 -0300

    Create desafio23.py

---
+++ desafio23.py
@@ -0,0 +1,3 @@
+""" Para este desafio crie um loop que peça para o usuário digitar o nome de uma fruta. Se a fruta 
+digitada não for abacate o loop deve continuar pedindo para o usuário digitar o nome de uma fruta 
+se a fruta for abacate o loop deve acabar e o programar imprimir parabéns você acertou a fruta """

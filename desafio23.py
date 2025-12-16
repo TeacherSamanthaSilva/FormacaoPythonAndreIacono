7e9eb2cdb923ce56235bea8a9a383ea49aa814f5
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-12-16 19:06:24 -0300

    Update desafio23.py

--- desafio23.py
+++ desafio23.py
@@ -1,3 +1,11 @@
 """ Para este desafio crie um loop que peça para o usuário digitar o nome de uma fruta. Se a fruta 
 digitada não for abacate o loop deve continuar pedindo para o usuário digitar o nome de uma fruta 
 se a fruta for abacate o loop deve acabar e o programar imprimir parabéns você acertou a fruta """
+
+fruta = "abacate"
+
+
+while fruta != "abacate":
+    print("Adivinnhe em qual fruta estou pensando")
+if fruta == "abacate":
+    print("Parabéns você acertou a fruta")

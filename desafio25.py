3168c71b6139410e5e81701672139290209b87c2
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-12-20 17:41:49 -0300

    Update desafio25.py

--- desafio25.py
+++ desafio25.py
@@ -3,3 +3,9 @@ Se a cidade estiver na lista imprima. A cidade está na lista de cidades. Se a c
 Imprima a cidade não está na lista de cidades OBS: Pode usar tupla em vez de lista """
 
 cidades = ("Lages", "Florianópolis","Pomerode")
+cidade_digitada = str(input("Digite o nome de qualquer cidade: "))
+
+if cidade_digitada in cidades:
+    print("A cidade está na lista de cidades.")
+else:
+    print("A cidade não está na lista de cidades")

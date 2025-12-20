b57f8298ce0a3121f03d52f3d26224d21e56ab00
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-12-20 17:34:29 -0300

    Create desafio25.py

---
+++ desafio25.py
@@ -0,0 +1,3 @@
+""" Para este desafio crie uma lista com três cidades. Peça ao usuário para digitar o nome de uma cidade
+Se a cidade estiver na lista imprima. A cidade está na lista de cidades. Se a cidade não estiver na lista 
+Imprima a cidade não está na lista de cidades OBS: Pode usar tupla em vez de lista """

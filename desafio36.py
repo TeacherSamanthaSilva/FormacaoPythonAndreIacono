5a8a9f8671f5e967a469b2fbe7d5701b312fa185
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-12-22 22:33:05 -0300

    Create desafio36.py

---
+++ desafio36.py
@@ -0,0 +1,3 @@
+""" Para este desafio crie uma lista com 5 nomes de países e as capitais desses países. Peça ao 
+usuário para digitar o nome de um país. Se o pais estiver na lista imprima a capital do pais é
+Se o pais não estiver na lista imprima. Desculpe não temos informação sobre a capital deste pais """

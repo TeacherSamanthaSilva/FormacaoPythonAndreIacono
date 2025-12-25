bd103d78bc1efd0faa31e83006d65d0665d4aa77
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-12-25 00:17:16 -0300

    desafios

---
+++ desafio27.py
@@ -0,0 +1,3 @@
+""" Para este desafio crie dois conjuntos, cada um contendo 5 nomes, Alguns nomes devem estar presentes
+em ambos os conjuntos. Use um métódo para encontrar quais nomes aparecem em ambos os conjuntos
+e imprima o resultado """

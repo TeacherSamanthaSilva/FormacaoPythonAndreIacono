c608a9305c07abe520451366c23b41cb76bacae0
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-12-25 00:34:58 -0300

    Update desafio27.py

--- desafio27.py
+++ desafio27.py
@@ -1,3 +1,15 @@
 """ Para este desafio crie dois conjuntos, cada um contendo 5 nomes, Alguns nomes devem estar presentes
 em ambos os conjuntos. Use um métódo para encontrar quais nomes aparecem em ambos os conjuntos
 e imprima o resultado """
+
+friends1 = {"Guilherme","Gabriel","Camila","Milena","Matheus"}
+friends2 = {"João","Milena","Gabriel","Ulysses","Leonardo"}
+
+interseccao = friends1.intersection(friends2)
+print(interseccao)
+
+uniao = friends1.union(friends2)
+print(uniao)
+
+diferenca = friends1.difference(friends2)
+print(diferenca)

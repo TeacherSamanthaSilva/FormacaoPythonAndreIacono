4104362652633de869d9cbf8ea3a33b9d53b65ed
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-12-25 17:55:59 -0300

    Update desafio28.py

--- desafio28.py
+++ desafio28.py
@@ -1,1 +1,7 @@
 """ Crie uma função que aceita um número como entrada e retorna o quadrado desse número """
+
+def quadradodenumero(numero):
+    return numero * numero
+
+numero = int(input("Digite um número qualquer: "))
+

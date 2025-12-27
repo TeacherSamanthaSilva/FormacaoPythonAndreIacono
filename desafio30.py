1e13f2550890b4f616bcc6b07b2e55dbb223f0a8
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-12-27 17:26:32 -0300

    Update desafio30.py

--- desafio30.py
+++ desafio30.py
@@ -1,2 +1,11 @@
 # Para este desafio crie uma função que calcule a potência de um número: A função deve aceitar dois 
 # argumentos a base e o expoente. 
+
+def potencia(base, expoente):
+    resultado = base ** expoente
+    return resultado
+
+base = int(input("Digite o valor da base"))
+expoente = int(input("Digite o valor do expoente"))
+
+print("O resultado de {base} elavado no {expoente} é {potencia(base,expoente)}")

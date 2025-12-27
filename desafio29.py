844d1e9680369b3c976ac4978632cd1406d9f4a3
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-12-27 17:18:31 -0300

    Update desafio29.py

--- desafio29.py
+++ desafio29.py
@@ -4,3 +4,9 @@
 def soma(num1,num2):
     resultado = num1 + num2
     return resultado
+
+num1 = int(input("Digite um número qualquer: "))
+num2 = int(input("Digite um número qualquer: "))
+
+
+print(f"A soma de {num1} e {num2} é {soma(num1,num2)}")

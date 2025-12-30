114338b1cbf51fde1f8c6c73a8880dafbf54451f
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-12-30 19:05:17 -0300

    Update desafio31.py

--- desafio31.py
+++ desafio31.py
@@ -1,4 +1,11 @@
 # Crie uma função para calcular o fatorial de um número
 
 def fatorial(numero):
-    
+    if numero == 0 or numero == 1:
+        return 1
+    else:
+        return numero * fatorial(numero - 1)
+
+numero = int(input("Digite o número que você deseja calcular o fatorial: "))
+
+print(f"O fatorial de {numero} é {fatorial(numero)}")

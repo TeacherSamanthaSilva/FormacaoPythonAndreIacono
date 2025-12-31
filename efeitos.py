080a94297c4ae51356ea4ae7fb01e12f638e147b
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-12-30 23:09:07 -0300

    brilho

--- efeitos.py
+++ efeitos.py
@@ -2,6 +2,6 @@ from PIL import Image, ImageEnhance, ImageFilter, ImageOps, ImageDraw
 
 img = Image.open("eu.jpg").convert("RGB")
 
-img_cor = ImageEnhance.Color(img).enhance(1.4)
-img_cor.save("03_saturacao.jpg")
-img_cor.show()
+img_brilho = ImageEnhance.Brightness(img).enhance(1.1)
+img_brilho.save("04_brilho.jpg")
+img_brilho.show()

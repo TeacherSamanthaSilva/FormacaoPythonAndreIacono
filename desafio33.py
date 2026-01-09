376240829549350aeb4ac239cc12b543fbdebb79
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2026-01-08 22:47:57 -0300

    Update desafio33.py

--- desafio33.py
+++ desafio33.py
@@ -1,1 +1,6 @@
 # Para este desafio crie uma funçao lambda que aceite um número e retorne o cubo desse número. 
+
+cubo = lambda numero:  numero ** 3 
+
+numero = int(input("Digite um número qualquer "))
+print(f"O cubo de {numero} é {cubo(numero)}")

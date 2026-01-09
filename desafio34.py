efbb294837f9f0f59732831c536b393d328c4626
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2026-01-09 18:51:50 -0300

    Update desafio34.py

--- desafio34.py
+++ desafio34.py
@@ -1,1 +1,8 @@
 # Para este desafio crie uma função lambda que aceite dois números e retorne a multiplicação desses número
+
+multiplicacao = lambda numero1,numero2: numero1 * numero2
+
+numero1 = int(input("Digite o primeiro número "))
+numero2 = int(input("Digite o segundo número "))
+
+print(f"A multiplicação de {numero1} por {numero2} é {multiplicacao(numero1,numero2)}")

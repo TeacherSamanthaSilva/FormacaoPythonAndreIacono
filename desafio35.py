3df373f3bafec4aa3bd7fc2026fa2455d6229898
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2026-01-24 18:11:26 -0300

    Update desafio35.py

--- desafio35.py
+++ desafio35.py
@@ -1,2 +1,7 @@
 # Para este desafio crie uma função lambda que aceite um número e retorne Par para o número for para ou
 # retorne Impar se o número for ímpar
+
+par_ou_impar = lambda numero : 'Par' if numero % 2 == 0 else 'Impar'
+
+numero = int(input("Digite um número "))
+print(f" O número {numero} é {par_ou_impar(numero)}")

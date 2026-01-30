336ac0bd72deaaa9ded70426928bbf914956198d
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2026-01-30 18:50:11 -0300

    Create desafio36.py

---
+++ desafio36.py
@@ -0,0 +1,2 @@
+""" Para este desafio crie uma função lambda que eleve um número ao quadrado. Em seguida use essa função
+para calcular o quadrado de todos os números em uma lista usando um loop for """

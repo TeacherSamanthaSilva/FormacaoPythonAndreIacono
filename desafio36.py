2fd24fd5fc887e12d558ae5dfde126a97b250522
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2026-01-30 18:51:25 -0300

    Update desafio36.py

--- desafio36.py
+++ desafio36.py
@@ -1,2 +1,4 @@
 """ Para este desafio crie uma função lambda que eleve um número ao quadrado. Em seguida use essa função
 para calcular o quadrado de todos os números em uma lista usando um loop for """
+
+numeros = [1,2,3,4,5,6,7,8,9,10]
